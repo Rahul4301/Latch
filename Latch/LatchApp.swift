f27e14a27884a776c5515a32e2b4cc6c9fe754fc
Rahul Suthar <Suthar.rahul042@gmail.com>
2026-01-29 20:21:27 -0800

    Initial Commit

---
+++ Latch/LatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatchApp.swift
+//  Latch
+//
+//  Created by Rahul Suthar on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct LatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
